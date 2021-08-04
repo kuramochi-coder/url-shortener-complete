cd0181b1f73328e0ceac0791ed6e275ef450f39b
kuramochi <47944476+kuramochi-coder@users.noreply.github.com>
2021-08-04 13:03:09 +0800

    Fixed delete bug using onclick deleteHandeler
    Added onclick deleteHandeler to fix delete bug when jQuery function was changed to ES6 syntax

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,36 @@
-if (window.location.pathname == "/") {
-  $ondelete = $(".table tbody td a.delete");
-  $ondelete.click(() => {
-    var id = $(this).attr("data-id");
+// if (window.location.pathname == "/") {
+//   $ondelete = $(".table tbody td a.delete");
+//   $ondelete.click(() => {
+//     var id = $(this).attr("data-id");
 
-    var request = {
+//     var request = {
+//       // url: `http://localhost:5000/api/urls/${id}`,
+//       url: `https://my-tinyurl.herokuapp.com/api/urls/${id}`,
+//       method: "DELETE",
+//     };
+
+//     if (confirm("Delete url record?")) {
+//       $.ajax(request).done((response) => {
+//         alert("Data Deleted Successfully!");
+//         location.reload();
+//       });
+//     }
+//   });
+// }
+
+const deleteHandeler = (element) => {
+  id = $(element).attr("data")
+  
+  var request = {
       // url: `http://localhost:5000/api/urls/${id}`,
       url: `https://my-tinyurl.herokuapp.com/api/urls/${id}`,
       method: "DELETE",
     };
 
     if (confirm("Delete url record?")) {
-      $.ajax(request).done((response) => {
+      $.ajax(request).done(function (response) {
         alert("Data Deleted Successfully!");
         location.reload();
       });
     }
-  });
 }
