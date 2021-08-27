670768d752a1c6e322f78b986483f95e389a0e6b
kuramochi-coder <raphael.kw@gmail.com>
2021-08-27 16:37:33 +0800

    added search bar feature and functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,19 +18,35 @@
 //   });
 // }
 
-const deleteHandler = (element) => {
-  id = $(element).attr("data")
-  
-  var request = {
-      // url: `http://localhost:5000/api/urls/${id}`,
-      url: `https://my-tinyurl.herokuapp.com/api/urls/${id}`,
-      method: "DELETE",
-    };
+const searchHandler = (element) => {
+  value = $(element).val().toLowerCase();
 
-    if (confirm("Delete url record?")) {
-      $.ajax(request).done(function (response) {
-        alert("Data Deleted Successfully!");
-        location.reload();
-      });
-    }
-}
+  $("#urlTable tr").filter(function () {
+    $(this).toggle($(this).text().toLowerCase().includes(value));
+  });
+};
+
+const clearSearchHandler = () => {
+  value = $("#search").val("");
+
+  $("#urlTable tr").filter(function () {
+    $(this).toggle(!$(this).text().toLowerCase().includes(value));
+  });
+};
+
+const deleteHandler = (element) => {
+  id = $(element).attr("data");
+
+  var request = {
+    // url: `http://localhost:5000/api/urls/${id}`,
+    url: `https://my-tinyurl.herokuapp.com/api/urls/${id}`,
+    method: "DELETE",
+  };
+
+  if (confirm("Delete url record?")) {
+    $.ajax(request).done(function (response) {
+      alert("Data Deleted Successfully!");
+      location.reload();
+    });
+  }
+};
